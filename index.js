f569df2317c38f7f02c43439dd2f176746ccfbfe
ivij <53103290+ivij@users.noreply.github.com>
2022-10-14 22:53:36 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 const port1 = 3000;
-var lighteData = 0;
+var lightData = 0;
 
 const SerialPort = require('serialport');
 const Readline = SerialPort.parsers.Readline;
@@ -9,9 +9,9 @@ const port = new SerialPort('COM5', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
 parser.on('data', (ligh) => {
     console.log(ligh);
-    ligheData = ligh;
+    lightData = ligh;
     app.get('/', (req, res) => {
-        res.send(lighData)
+        res.send(lightData)
     })
 });
 
